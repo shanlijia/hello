ec1bec1e65312f83947ff494b679aab1c9387d2e
shanlijia <shanlijia@163.com>
2020-11-16 18:11:48 +0800

    test3333

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello wold\n");
+        System.out.println("你好ya");
     }
 }
