0c450fa18e8a98ce7beaa1299d32132192175e35
shanlijia <shanlijia@163.com>
2020-11-16 18:02:52 +0800

    test

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * @author slj
+ * @create 2020/11/16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
