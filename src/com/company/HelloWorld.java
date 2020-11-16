b70793ec604be9bd8a69f19453ace5c2948402b8
shanlijia <shanlijia@163.com>
2020-11-16 18:08:07 +0800

    test2

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -7,5 +7,6 @@ package com.company;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello wold\n");
     }
 }
